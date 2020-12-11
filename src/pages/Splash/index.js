bc747b152cad53b51bdbcde67fa80d558f4ab77c
DiegoCayun <peridiegoc@hotmail.com>
2020-12-11 20:27:16 -0300

    [FIXED] - auto push location

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SplashStyled, Row1, Row2, Row3 } from './splash.styles';
+import { useLocation } from 'wouter';
 
 const Splash = () => {
+    const [, pushLocation ] = useLocation();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            pushLocation('/onboarding');
+        }, 5000);
+        return () => clearTimeout(timer);
+      }, []);
+
     return (
         <SplashStyled>
             <Row1/>
